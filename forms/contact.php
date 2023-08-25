63bce7f33ce9b5ed64b8b5c7fba765c004103369
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-08-25 14:39:58 +0300

    translate testimonals and contacts

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,18 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = 'itSushkoAndriy@gmail.com'; // Адреса отримувача
+    $subject = $_POST['subject']; // Тема повідомлення
+    $message = $_POST['message']; // Тіло повідомлення
+    $headers = "From: " . $_POST['email'] . "\r\n"; // Адреса відправника
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    // Відправити лист
+    $mail_sent = mail($to, $subject, $message, $headers);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Перевірка, чи лист успішно надіслано
+    if ($mail_sent) {
+        echo "Your message has been sent. Thank you!";
+    } else {
+        echo "Error: Unable to send message.";
+    }
+}
 ?>
